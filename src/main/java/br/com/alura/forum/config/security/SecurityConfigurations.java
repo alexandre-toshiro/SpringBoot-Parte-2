e6cb3b8c03046aef5cc7e4e95777608d50e3ae5d
Alexandre Toshiro <atoshiro@indracompany.com>
2020-03-16 10:14:19 -0300

    Autenticando o usuario / authService / bcrypt

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity // Habilita o security do spring
 @Configuration // Para o spring já carregar e ler as configurações no projeto ao ser inicializado.
@@ -14,11 +17,15 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	// Por padrão o spring bloqueia tudo, até que se venha e faça todas as configurações necessárias
 	// liberando os acessos
 	
+	@Autowired
+	private AutentificacaoService autenticacaoService;
+	
 	
 	// Configurações de autenticação.
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
+		// passamos qual a classe possui a lógica de auth.
 	}
 	
 	// Configurações de autorização.
@@ -37,6 +44,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	public void configure(WebSecurity web) throws Exception {
 		
 	}
+	
+	
 } 
 
 
